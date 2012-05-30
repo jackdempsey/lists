2f9a15110c632b217adb39220c52d5cc60732bb8
Jack Dempsey <jack.dempsey@gmail.com>
2012-05-30 10:32:49 -0700

    reject update if subject and body are both blank

--- app/models/lists/list.rb
+++ app/models/lists/list.rb
@@ -5,6 +5,6 @@ module Lists
 
     attr_accessible :body, :position, :subject, :listable, :list_items_attributes
 
-    accepts_nested_attributes_for :list_items
+    accepts_nested_attributes_for :list_items, reject_if: ->(o) {o[:subject].blank? and o[:body].blank?}, allow_destroy: true
   end
 end

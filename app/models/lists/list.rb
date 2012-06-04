ef0405fdc22cee2ccaab2712e3d340d769e44be4
Jack Dempsey <jack.dempsey@gmail.com>
2012-06-04 10:02:45 -0700

    need to accept listable_type and listable_id in mass attributes

--- app/models/lists/list.rb
+++ app/models/lists/list.rb
@@ -3,7 +3,7 @@ module Lists
     has_many :list_items
     belongs_to :listable, polymorphic: true
 
-    attr_accessible :body, :position, :subject, :listable, :list_items_attributes
+    attr_accessible :body, :position, :subject, :listable, :listable_type, :listable_id, :list_items_attributes
 
     accepts_nested_attributes_for :list_items, reject_if: ->(o) {o[:subject].blank? and o[:body].blank?}, allow_destroy: true
   end

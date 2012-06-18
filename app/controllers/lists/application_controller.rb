593bb8521eb83b691745b07505802e5e2692a416
Jack Dempsey <jack.dempsey@gmail.com>
2012-06-18 00:35:31 -0700

    Setup basic access callback to restrict who can see lists

--- app/controllers/lists/application_controller.rb
+++ app/controllers/lists/application_controller.rb
@@ -1,4 +1,13 @@
 module Lists
   class ApplicationController < ActionController::Base
+    before_filter :authenticate_user
+
+    protected
+
+    def authenticate_user
+      unless Lists.access_callback.call
+        raise "Authentication failed."
+      end
+    end
   end
 end
--- lib/lists.rb
+++ lib/lists.rb
@@ -1,6 +1,13 @@
 require "lists/engine"
+require 'active_support/dependencies'
 require 'haml'
 require 'acts_as_list'
 
 module Lists
+  mattr_accessor :access_callback
+  @@access_callback = -> { respond_to?(:current_admin_user) ? current_admin_user : false }
+
+  def self.setup
+    yield self
+  end
 end
